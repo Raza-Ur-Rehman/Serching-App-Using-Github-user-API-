798039b8f7257eef94b137b41a18a958a160e4fb
Raza ur Rehman <mrazaurrehman12345@gmail.com>
2024-07-28 03:52:19 +0500

    Update app.js

--- app.js
+++ app.js
@@ -15,6 +15,7 @@ function ApiCall() {
       return response.json();
     })
     .then((data) => {
+        console.log(data);
       displayData(data);
     })
     .catch((error) => {
@@ -22,13 +23,36 @@ function ApiCall() {
     });
 }
 btn.addEventListener("click", getData);
-search.addEventListener("keyup", (event) => { 
-     event.key === "Enter"
+search.addEventListener("keyup", (event) => {
+    if (event.key === "Enter") {
       getData();
+    }
+  
 });
 
 
-
+function displayData(data) {
+    const userDiv = document.getElementById("box");
+    // userDiv.innerHTML = "";
+    userDiv.innerHTML = `
+    <div class="card">
+      <img src="${data.avatar_url}" alt="User Avatar" class="card-img">
+      <div class="profile-info">
+        <h2 class="title">${data.name}</h2>
+        <p class="user-name">${data.login}</p>
+        <p class="bio"> ${data.bio}</p>
+        <div class="follow-box">
+                    <i class="fa-solid fa-users"></i>
+                    <span>${data.followers} Follower</span>
+                    <span>${data.following} Following</span>
+                    </div>
+                    <p><i class="fa-solid fa-location-dot"></i> ${data.location}</p>
+        <a href="${data.html_url}" id="profile-link">Follow</a>
+      </div>
+    </div>
+    `;
+    search.value = "";
+}
 
 
 
